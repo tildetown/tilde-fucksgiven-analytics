320328a1164093023611aab29b86944208acf49b
Tara Planas <tara.planas@gmail.com>
2018-12-20 20:41:03 -0800

    Vendoring

---
+++ vendor/github.com/franela/goblin/resolver_test.go
@@ -0,0 +1,18 @@
+package goblin
+
+import "testing"
+
+func TestResolver(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("Resolver", func() {
+		g.It("Should resolve the stack until the test", func() {
+			dummyFunc(g)
+		})
+	})
+}
+
+func dummyFunc(g *G) {
+	stack := ResolveStack(3)
+	g.Assert(len(stack)).Equal(5)
+}
